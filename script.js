fdf381cbc2fba5fc31558b71c037f51390a66a09
abnguessan <91183238+abnguessan@users.noreply.github.com>
2022-04-25 15:29:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -22,15 +22,6 @@ document.querySelector('.booked .close-btn2').addEventListener('click', function
 });
 
 
-/*Responsiveness*/
-
-$('#menu-btn').click(function(){
-    $('nav .navigation ul').addClass('active')
-});
-$('#menu-close').click(function(){
-    $('nav .navigation ul').removeClass('active')
-});
-
 /* Aboubacar Nguessan 
 CSCE 3420 Internet Programming 
 Prof: Dr Sultan Al Saif
